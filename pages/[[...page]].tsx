f6fb9c16572191d86d0e1fdf5adec94b7339b995
Mihir Sidhdhapura <mihir.s@squareboat.com>
2023-07-28 13:24:51 +0530

    INIT:

--- pages/[[...page]].tsx
+++ pages/[[...page]].tsx
@@ -8,36 +8,38 @@ import "../builder.config"
 import LanguageSwitch from '@/components/LanguageSwitch';
 
 // Initialize the Builder SDK with your public API key
-if (!process.env.NEXT_PUBLIC_BUILDER_API_KEY) {throw new Error('Missing NEXT_PUBLIC_BUILDER_API_KEY env var')}
+if (!process.env.NEXT_PUBLIC_BUILDER_API_KEY) { throw new Error('Missing NEXT_PUBLIC_BUILDER_API_KEY env var') }
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
 
 
 // Define a function that fetches the Builder 
 // content for a given page
 export async function getStaticProps({ params, locale }: GetStaticPropsContext) {
-    // Fetch the builder content for the given page
-    
-    const page = await builder
-      .get('page', {
-        userAttributes: {
-          urlPath:
-            params && Array.isArray(params?.page) && params.page.length > 0
-              ? '/' + params.page.join('/')
-              : '/', // Make sure the index page has a urlPath of '/'
-        },
-      })
-      .toPromise();
-  
-    // Return the page content as props
-    return {
-      props: {
-        page: page || null,
-        locale: locale || null,
+  // Fetch the builder content for the given page
+
+  const page = await builder
+    .get('page', {
+      userAttributes: {
+        urlPath:
+          params && Array.isArray(params?.page) && params.page.length > 0
+            ? '/' + params.page.join('/')
+            : '/', // Make sure the index page has a urlPath of '/'
       },
-      // Revalidate the content every 5 seconds
-      revalidate: 5,
-    };
-  }
+    })
+    .toPromise();
+
+
+
+  // Return the page content as props
+  return {
+    props: {
+      page: page || null,
+      locale: locale || null,
+    },
+    // Revalidate the content every 5 seconds
+    revalidate: 5,
+  };
+}
 
 // Define a function that generates the 
 // static paths for all pages in Builder
@@ -45,19 +47,19 @@ export async function getStaticPaths() {
   // Get a list of all pages in Builder
   const pages = await builder.getAll('page', {
     // We only need the URL field
-    fields: 'data.url', 
+    fields: 'data.url',
     options: { noTargeting: true },
   });
 
   // Generate the static paths for all pages in Builder
   return {
-    paths: pages.map((page) => ({
+    paths: pages.map((page: any) => ({
       params: { page: page.data?.url?.slice(1).split('/') }, // Remove the leading slash and split the URL into an array of strings
     })),
     fallback: true,
   };
 }
-  
+
 
 // Define the Page component
 export default function Page({ page, locale }: any) {
