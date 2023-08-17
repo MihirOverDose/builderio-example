b189a61c47a6a05f9ffe7bed6ab0aa1259a69787
Mihir Sidhdhapura <mihir.sidhdhpura@overdose.digital>
2023-08-17 16:06:17 +0530

    FIXED: build issue fixed

--- config/index.ts
+++ config/index.ts
@@ -2,15 +2,15 @@ import constants from "@/constants"
 
 type ConfigType  ={
     endPoints: {
-        productAppBaseURL?: string ,
-        productDetail?: (slug: string) => string
+        productAppBaseURL: string ,
+        productDetail: (slug: string) => string
     }
 }
 
 const config : ConfigType= {
     endPoints: {
-        productAppBaseURL: process.env.NEXT_PUBLIC_PRODUCT_BASE_URL,
-        productDetail : (slug: string) => `${process.env.NEXT_PUBLIC_PRODUCT_BASE_URL}/Product/product/${slug}`
+        productAppBaseURL: process.env.NEXT_PUBLIC_PRODUCT_BASE_URL!,
+        productDetail : (slug: string) => `${process.env.NEXT_PUBLIC_PRODUCT_BASE_URL!}/Product/product/${slug}`
     }
 }
 
